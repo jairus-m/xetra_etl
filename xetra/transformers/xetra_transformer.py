d4027e43ada4c33ed175795f160b4fdb0ede8e6b
jairus-m <jairusmartinez@gmail.com>
2023-12-28 14:25:51 -0800

    profiler

--- xetra/transformers/xetra_transformer.py
+++ xetra/transformers/xetra_transformer.py
@@ -88,7 +88,8 @@ class XetraETL():
             self.src_args.src_first_extract_date, self.meta_key, self.s3_bucket_trg)
         self.meta_update_list = [date for date in self.extract_date_list\
             if date >= self.extract_date]
-
+    
+    @profile
     def extract(self):
         """
         Read the source data and concatenates them to one Pandas DataFrame
@@ -107,6 +108,7 @@ class XetraETL():
         self._logger.info('Extracting Xetra source files finished.')
         return data_frame
 
+    @profile
     def transform_report1(self, data_frame: pd.DataFrame):
         """
         Applies the necessary transformation to create report 1
@@ -174,6 +176,7 @@ class XetraETL():
         self._logger.info('Applying transformations to Xetra source data finished...')
         return data_frame
 
+    @profile
     def load(self, data_frame: pd.DataFrame):
         """
         Saves a Pandas DataFrame to the target
@@ -193,7 +196,8 @@ class XetraETL():
         MetaProcess.update_meta_file(self.meta_update_list, self.meta_key, self.s3_bucket_trg)
         self._logger.info('Xetra meta file successfully updated.')
         return True
-
+    
+    @profile
     def etl_report1(self):
         """
         Extract, transform and load to create report 1
